48d07d9a9d626a7e602d5d4d5b5d2bb412be6322
Andrius007 <79835280+Andrius007@users.noreply.github.com>
2021-03-19 15:45:51 +0300

    Выполнение первой задачи по Спрингу

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pojo.Cat;
 
 public class App {
     public static void main(String[] args) {
@@ -8,5 +9,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat barsikCat = (Cat) applicationContext.getBean("grom");
+        System.out.println(barsikCat.getName());
+        Cat pushokCat =
+                (Cat) applicationContext.getBean("priz");
+        System.out.println(pushokCat.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(barsikCat == pushokCat);
     }
 }
